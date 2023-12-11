214b003c76f0219a917d1dfd4ea528c7f63cceef
shuntagami <shuntagami23@gmail.com>
2023-12-11 16:48:24 +0900

    use stream

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import ExcelJS from "exceljs";
-import { readFileSync } from "fs";
+import { createWriteStream } from "fs";
 import { execSync } from 'child_process';
+import { copyRows } from "./utils/excel_util";
+import { exit } from "process";
 
 async function main() {
-  const workbook = new ExcelJS.Workbook();
+  const path = `./results/${Date.now()}.xlsx`;
+  const stream = createWriteStream(path)
+  const options = {
+    useStyles: true,
+    stream
+  }
+  const template = await new ExcelJS.Workbook().xlsx.readFile('./templates/template.xlsx')
+  const sourceSheet = template.worksheets[0]
+  if (sourceSheet === undefined ) {
+    exit(1)
+  }
+  const workbook = new ExcelJS.stream.xlsx.WorkbookWriter(options)
   const stdout = execSync('ulimit -n').toString().trim();
   const ulimitN = parseInt(stdout, 10);
   console.log(ulimitN);
 
   const targetSheet = workbook.addWorksheet("Target Sheet");
+  copyRows(targetSheet, sourceSheet, 1, 20, 1)
 
   for (let i = 0; i < 1000; i++) {
     const imagePath = `images/image-${i}.jpeg`
@@ -25,10 +39,8 @@ async function main() {
     });
 
   }
-  const path = `./results/${Date.now()}.xlsx`;
   console.log(`writing data to ${path}`);
-
-  await workbook.xlsx.writeFile(path)
+  await workbook.commit()
   console.log(`File written to ${path}`);
 }
 
